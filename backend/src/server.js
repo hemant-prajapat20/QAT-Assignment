53d1d67003ee688fe8f02af0f8332a756f855e0a
hemant-prajapat20 <prajapathemant564@gmail.com>
2026-02-27 13:22:01 +0530

    #

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,12 +5,23 @@ import mongoose from "mongoose";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
